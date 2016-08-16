626842f98004c8ec826d3818073f78dbc316180c
Hudson Prestidge <charcon@ihug.co.nz>
2016-08-17 11:15:10 +1200

    complete lesson 6

--- lessons/06-params/modules/Repos.js
+++ lessons/06-params/modules/Repos.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router'
+
 
 export default React.createClass({
   render() {
-    return <div>Repos</div>
+    return (
+      <div>
+      <h2>Repos</h2>
+
+      <ul>
+        <li><Link to="/repos/reactjs/React-Router">React Router</Link></li>
+        <li><Link to="/repos/facebook/React">React</Link></li>
+      </ul>
+
+      </div>
+    )
   }
 })
