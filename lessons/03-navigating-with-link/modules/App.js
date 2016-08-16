054cbf81290c6f45bb4e6bd5b1559fafcbcf0f99
Hudson Prestidge <charcon@ihug.co.nz>
2016-08-17 10:44:40 +1200

    complete first three exercises

--- lessons/03-navigating-with-link/modules/App.js
+++ lessons/03-navigating-with-link/modules/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default React.createClass({
   render() {
-    return <div>Hello, React Router!</div>
+    return(
+    <div>
+      <div>Hello, React Router!</div>
+      <ul>
+        <li><Link to='/About'> About </Link></li>
+        <li><Link to='/Repos'> Repos </Link></li>
+      </ul>
+    </div>
+    )
   }
 })
